ba385d5e75006637cb965fe287b6f0c5407e14ca
Shinsuke KUROKI <s-kuroki@aiming-inc.com>
2014-02-01 14:43:15 +0900

    Generate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActiveAdminSandbox::Application.config.secret_key_base = '2f3b5653e20826c9e8ed53764d2dd90e33a04d095756b55d56fc47b503e9abbc26efe674c8d0abc6d60fb4b3efa42735c8009e225265f96ff8e880f2cbe07b6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ActiveAdminSandbox::Application.config.session_store :cookie_store, key: '_active_admin_sandbox_session'
